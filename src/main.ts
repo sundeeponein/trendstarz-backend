96675cb3668e315f9e2ed90335946f8461575cfb
arkhya <venkatnarayana@arkhyatech.net>
2025-11-25 08:49:20 +0530

    nest and mongoodb

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
+  // Connect to MongoDB
+  try {
+    await mongoose.connect(process.env.MONGODB_URI as string);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
