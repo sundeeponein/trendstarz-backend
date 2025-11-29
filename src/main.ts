cfd02aef94cb9d9f637ad6edd2a0170caac33a23
arkhya <venkatnarayana@arkhyatech.net>
2025-11-29 09:18:00 +0530

    render port fix backend

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,23 @@
+// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-
-  // ---- Seed ONLY when RUN_SEED=true ----
-  if (process.env.RUN_SEED === 'true') {
-    console.log('🚀 RUN_SEED=true — Running seeder.js...');
-    const { seedDatabase } = await import('./seeder.js');
-    await seedDatabase();
-    console.log('✅ Seed completed successfully.');
-    // IMPORTANT: Return here so the server doesn't run twice
-    return;
-  }
-
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: true, credentials: true });
 
-  console.log('DEBUG MONGODB_URI:', process.env.MONGODB_URI);
+  app.enableCors({
+    origin: [
+      'https://trendstarz.in',
+      'https://www.trendstarz.in',
+      'http://localhost:4200' // only for local dev
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
-
   console.log('Server running on port:', port);
 }
 
