dd716b2a4a294cbef19bcbc5a82852c1a98694a2
arkhya <venkatnarayana@arkhyatech.net>
2025-12-02 20:11:17 +0530

    updated cloudinary set

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { CloudinaryService } from '../cloudinary.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, CloudinaryService],
 })
 export class UsersModule {}
